cf3478a054da0d7ee74f5209b40dc49ec03e573d
jeffbhove-at-957077460593 <jeff.bhove@gmail.com>
2019-05-31 12:17:30 -0400

    Changed names and added add subscription

---
+++ add.js
@@ -0,0 +1,30 @@
+'use strict';
+const doc = require('dynamodb-doc');
+const dynamo = new doc.DynamoDB();
+
+const uuidv1 = require('uuid/v1');
+
+exports.handler = (event, context, callback) => {
+    console.log(uuidv1());
+    dynamo.putItem({
+        TableName: "product-subscriptions",
+        Item: {
+            id: uuidv1(),
+            "email": event.email,
+            "productCode": event.productCode
+        }
+    }, function(err, data) {
+        if (err) {
+            console.log(err, err.stack);
+            callback(null, {
+                statusCode: '500',
+                body: err
+            });
+        } else {
+            callback(null, {
+                statusCode: '200',
+                body: data
+            });
+        }
+    });
+};
